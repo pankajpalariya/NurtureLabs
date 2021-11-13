2695a23c38e24a1b6385d41594b5593304cf6ab3
pankajpalariya <pankajpalariya21@gmail.com>
2021-11-13 09:13:13 +0530

    adf

--- project/settings.py
+++ project/settings.py
@@ -174,15 +174,16 @@ USE_TZ = True
 
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_URL='/media/'
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
+# # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# MEDIA_URL='/media/'
 
-MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+# MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
 
 
 #Activate Django-Heroku.
